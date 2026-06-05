7f1e65778e46fa914a4b5650cdab9b13b43cefb5
Demon6590 <dima.chernytin@gmail.com>
2026-06-05 13:43:38 +0300

    добовление логики к кнопкам

--- AccountsManagerApp.Desktop/Windows/RegistrationWindow/RegistrationWindow.axaml.cs
+++ AccountsManagerApp.Desktop/Windows/RegistrationWindow/RegistrationWindow.axaml.cs
@@ -1,6 +1,9 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
 using Avalonia.Markup.Xaml;
+
+using MsBox.Avalonia;
 
 namespace AccountsManagerApp.Desktop.Windows.RegistrationWindow;
 
@@ -9,5 +12,23 @@ public partial class RegistrationWindow : Window
     public RegistrationWindow()
     {
         InitializeComponent();
+    }
+
+    private void Button_Cancel_OnClick(object? sender, RoutedEventArgs e)
+    {
+        var Window = new AuthWindow();
+        Window.Show();
+        this.Close();
+
+    }
+
+    private async void Button_Login_OnClick(object? sender, RoutedEventArgs e)
+    {
+        if (Input_Password_Confirm.Value != Input_Password.Value)
+        {
+            await MessageBoxManager
+                .GetMessageBoxStandard("Ошибка", "Повторный пароль не соответствует паролю")
+                .ShowAsync();
+        }
     }
 }
