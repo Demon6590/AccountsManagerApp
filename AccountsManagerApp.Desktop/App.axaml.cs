c9b9b8289089b699c20dd5527cbcc860c59df219
Demon6590 <dima.chernytin@gmail.com>
2026-06-05 13:32:40 +0300

    добовление окни и нескольких компонентов

--- AccountsManagerApp.Desktop/App.axaml.cs
+++ AccountsManagerApp.Desktop/App.axaml.cs
@@ -1,3 +1,5 @@
+using AccountsManagerApp.Desktop.Windows.RegistrationWindow;
+
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
@@ -15,7 +17,7 @@ public partial class App : Application
     {
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
-            desktop.MainWindow = new AuthWindow();
+            desktop.MainWindow = new RegistrationWindow();
         }
 
         base.OnFrameworkInitializationCompleted();
